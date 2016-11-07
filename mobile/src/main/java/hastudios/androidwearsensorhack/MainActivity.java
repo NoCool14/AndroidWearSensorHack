46c7d407138ceef7774a0f144eee05d689edbdfd
NoCool <armen.seksenyan@gmail.com>
2016-11-06 18:20:26 -0800

    1.   Connects with the phone 2.   Sends the data using dataApi 3.   Stores in a text file for both gyroscope and accelerometer on different files. 4.   Displays the data on both the phone and wearable.

--- mobile/src/main/java/hastudios/androidwearsensorhack/MainActivity.java
+++ mobile/src/main/java/hastudios/androidwearsensorhack/MainActivity.java
@@ -1,10 +1,48 @@
 package hastudios.androidwearsensorhack;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.vision.text.Text;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.R.attr.data;
+import static android.R.attr.tag;
+
+public class MainActivity extends Activity implements
+                                                      GoogleApiClient.ConnectionCallbacks,
+                                                      GoogleApiClient.OnConnectionFailedListener
+{
+    private static final String TAG = "MOBILE_MAIN";
+
+    private static final String GYROLOG = "/Gyrolog";
+    private static final String ACCELLOG = "/Accellog";
+
+    File path;
+    TextView connectionStatus;
+
+
+    private GoogleApiClient mGoogleApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -13,5 +51,140 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
 
+        Log.d(TAG, "ON CREATE");
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+
+        mGoogleApiClient.connect();
+        connectionStatus = (TextView)findViewById(R.id.textView2);
+        TextView communicationTextView = (TextView)findViewById(R.id.textView3);
+
+        connectionStatus.setText("NOT CONNECTED");
+        communicationTextView.setText("NOTHING COMING IN YET");
+
+        path = getApplicationContext().getFilesDir();
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle)
+    {
+        Log.d(TAG, "INSIDE ONcONNECTED");
+        connectionStatus.setText("CONNECTED");
+        Wearable.DataApi.addListener(mGoogleApiClient, new SensorHackDataListener(getApplicationContext()));
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i)
+    {
+
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
+    {
+        Log.e(TAG, "Connection to Google API client has failed");
+
+    }
+
+    public class SensorHackDataListener implements DataApi.DataListener
+    {
+        Context context;
+        File gyroFile = new File(path, "gyroFile.txt");
+        File accelFile = new File(path, "accelFile.txt");
+        TextView gyroCommunicationTextView, accelCommunicationTextView;
+        public SensorHackDataListener(Context context)
+        {
+            this.context = context;
+            gyroCommunicationTextView = (TextView)findViewById(R.id.textView3);
+            accelCommunicationTextView = (TextView)findViewById(R.id.textView4);
+        }
+        @Override
+        public void onDataChanged(DataEventBuffer dataEventBuffer)
+        {
+            for (DataEvent event : dataEventBuffer) {
+                if (event.getType() == DataEvent.TYPE_CHANGED) {
+                    // DataItem changed
+                    DataItem item = event.getDataItem();
+                    if (item.getUri().getPath().compareTo(GYROLOG) == 0)
+                    {
+                        DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                        Log.d(TAG, "" + dataMap.getFloat("firstValue"));
+                        Log.d(TAG, "" + dataMap.getFloat("secondValue"));
+                        Log.d(TAG, "" + dataMap.getFloat("thirdValue"));
+                        String output = "GYROSCOPE "
+                                + " X: " + dataMap.getFloat("firstValue")
+                                + " Y: " + dataMap.getFloat("secondValue")
+                                + " Z: " + dataMap.getFloat("thirdValue");
+
+
+                       gyroCommunicationTextView.setText(output);
+                        FileOutputStream stream = null;
+                        try
+                        {
+                            stream = new FileOutputStream(gyroFile);
+                        }
+                        catch (FileNotFoundException e)
+                        {
+                            e.printStackTrace();
+                        }
+                        try
+                        {
+                            stream.write(output.getBytes());
+                            stream.close();
+                        }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    else if(item.getUri().getPath().compareTo(ACCELLOG) == 0)
+                    {
+                        DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                        Log.d(TAG, "" + dataMap.getFloat("firstValue"));
+                        Log.d(TAG, "" + dataMap.getFloat("secondValue"));
+                        Log.d(TAG, "" + dataMap.getFloat("thirdValue"));
+                        String output = "ACCELEROMETER "
+                                + " X: " + dataMap.getFloat("firstValue")
+                                + " Y: " + dataMap.getFloat("secondValue")
+                                + " Z: " + dataMap.getFloat("thirdValue");
+
+                        accelCommunicationTextView.setText(output);
+                        FileOutputStream stream = null;
+                        try
+                        {
+                            stream = new FileOutputStream(accelFile);
+                        }
+                        catch (FileNotFoundException e)
+                        {
+                            e.printStackTrace();
+                        }
+                        try
+                        {
+                            stream.write(output.getBytes());
+                            stream.close();
+                        }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                } else if(event.getType() == DataEvent.TYPE_DELETED)
+                {
+                    // DataItem deleted
+                }
+            }
+
+        }
     }
 }
--- wear/src/main/java/hastudios/androidwearsensorhack/MainActivity.java
+++ wear/src/main/java/hastudios/androidwearsensorhack/MainActivity.java
@@ -1,14 +1,55 @@
 package hastudios.androidwearsensorhack;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import static android.Manifest.permission_group.STORAGE;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+import static com.google.android.gms.internal.zzs.TAG;
 
 public class MainActivity extends Activity
 {
 
+    private static final String GYROLOG = "/Gyrolog";
+    private static final String ACCELLOG = "/Accellog";
     private TextView mTextView;
+
+
+    private SensorManager mSensorManager;
+    private Sensor sGyroscope, sAccelerometer;
+
+    private double accelerometerVectorOne = 0;
+    private double accelerometerVectorTwo = 0;
+
+    private double gyrometerVectorOne = 0;
+    private double gyrometerVectorTwo = 0;
+
+    GoogleApiClient mGoogleApiClient;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,6 +66,115 @@ public class MainActivity extends Activity
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
-        
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .build();
+        mGoogleApiClient.connect();
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        getSensors();
+
+        sGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        sAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mSensorManager.registerListener(new GyroscopeSensorListener(getApplicationContext()), sGyroscope, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(new AccelerometerSensorListener(getApplicationContext()), sAccelerometer, 50000);
     }
+
+
+    private void getSensors() {
+
+        final List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor type : deviceSensors) {
+            Log.i("sensors", type.getName().toString());
+        }
+    }
+
+
+    private class GyroscopeSensorListener implements SensorEventListener
+    {
+
+
+        Context context;
+        public GyroscopeSensorListener(Context context)
+        {
+            this.context = context;
+        }
+        @Override
+        public void onSensorChanged(SensorEvent event)
+        {
+
+
+            if (Math.abs(event.values[0] - gyrometerVectorOne) > 0.1 && Math.abs(event.values[1] - gyrometerVectorTwo) > 0.1)
+            {
+                TextView gyroText = (TextView) findViewById(R.id.Gyro);
+                gyroText.setText("GYROSCOPE \n X: " + event.values[0] + "\n Y: " + event.values[1] + "\n Z: " + event.values[2]);
+                sendToPhone(GYROLOG,event.values[0], event.values[1], event.values[2]);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    }
+
+    private class AccelerometerSensorListener implements SensorEventListener {
+        Context context;
+        public AccelerometerSensorListener(Context context)
+        {
+            this.context = context;
+        }
+
+
+
+        @Override
+        public void onSensorChanged(SensorEvent event)
+        {
+            if (Math.abs(event.values[0] - accelerometerVectorOne) > 0.1 && Math.abs(event.values[1] - accelerometerVectorTwo) > 0.1)
+            {
+                accelerometerVectorOne = event.values[0]; accelerometerVectorTwo = event.values[1];
+                TextView accelerometerTextView= (TextView)findViewById(R.id.Accel) ;
+                accelerometerTextView.setText("ACCELEROMETER \nX: " + event.values[0] + "\nY: " + event.values[1] + "\nZ: " + event.values[2]);
+                sendToPhone(ACCELLOG,event.values[0], event.values[1], event.values[2]);
+
+
+            }
+
+        }
+
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+
+    }
+    private void sendToPhone(String sensor, float firstValue, float secondValue, float thirdValue) {
+        Log.d("SEND TO PHONE", "sendToPhone: ");
+        PutDataMapRequest putDataMapRequest = PutDataMapRequest.create(sensor);
+        putDataMapRequest.getDataMap().putFloat("firstValue", firstValue);
+        putDataMapRequest.getDataMap().putFloat("secondValue", secondValue);
+        putDataMapRequest.getDataMap().putFloat("thirdValue", thirdValue);
+        PutDataRequest request = putDataMapRequest.asPutDataRequest();
+
+
+        if (!mGoogleApiClient.isConnected())
+        {
+            Log.d("SEND TO PHONE", "!CONNECTED");
+            return;
+        }
+
+        Wearable.DataApi.putDataItem(mGoogleApiClient, request).setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
+            @Override
+            public void onResult(DataApi.DataItemResult dataItemResult) {
+                Log.d(TAG, "attempt to put data was a " +
+                        (dataItemResult.getStatus().isSuccess()? "success!"
+                                : "failure =( " + dataItemResult.getStatus().getStatusCode()));
+            }
+        });
+    }
+
 }
